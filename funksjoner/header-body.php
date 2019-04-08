0c3d682b285cdee31fa595d19b5213f84d7aefef
bereketgadhanom <bereketgadhanom@gmail.com>
2019-04-09 00:19:39 +0200

    Added jquery-ui-min

--- funksjoner/header-body.php
+++ funksjoner/header-body.php
@@ -6,12 +6,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title> Havnehotell funksjon </title>
-        <link rel="stylesheet" type="text/css" href="../css/hh-stil.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+        <link rel="stylesheet" type="text/css" href="../css/hh-stil.css">
+
+
         <?php include('metadata.php'); ?> <br>
     </head>
     <body>
         <header>
            <h1> . </h1>
-           <nav>  <a class="currentlink" href="login"> Login </a>  </nav>
+           <nav>  <a class="currentlink" href="login.php"> Login </a>  </nav>
         </header>
+        
