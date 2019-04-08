66495176a4dbfd26b7dd61b37b50f089fcab0e6f
bereketgadhanom <bereketgadhanom@gmail.com>
2019-04-09 00:14:43 +0200

    Deleting implode function and create errors file

--- funksjoner/done.php
+++ funksjoner/done.php
@@ -1,17 +1,14 @@
 <?php session_start(); ?>
-<?php require('header-body.php');
-
-  ?>
+<?php require('header-body.php');  ?>
     <main>
         <?php
-         // else {echo "<p> <a href='index.php'>Vennligst fyll ut skjemaet på nytt.</a></p>"; }
-          // Hvis knappen er trykket
             if(empty($_SESSION['email'])) {
               echo "<p> <a href='index.php'>Vennligst fyll ut skjemaet på nytt.</a></p>"; }
               else {
+             
                 //Henter databasetilkoblingen
                 require("db.php");
-                //Vaske data for SQL Injections
+                //Vaske data for SQL-angrep
                 $navn_clean = mysqli_real_escape_string($db_con, $_SESSION['name']);
                 $epost_clean = mysqli_real_escape_string($db_con, $_SESSION['email']);
                 $phone_clean = mysqli_real_escape_string($db_con, $_SESSION['phone']);
@@ -21,36 +18,32 @@
                 $antall_rom_clean = mysqli_real_escape_string($db_con, $_SESSION['quantity']);
                 $rom_clean = mysqli_real_escape_string($db_con, $_SESSION['room_number']);
                 $people_clean = mysqli_real_escape_string($db_con, $_SESSION['people']);
-                $maaltid_clean = mysqli_real_escape_string($db_con, $_SESSION['meals']);
-                  $mt = implode(', ', $maaltid_clean);
+                $lunsj_clean = mysqli_real_escape_string($db_con, $_SESSION['lunch']);
+                $middag_clean = mysqli_real_escape_string($db_con, $_SESSION['dinner']);
                 $parkering_clean = mysqli_real_escape_string($db_con, $_SESSION['parking']);
                 $parkering_id_clean = mysqli_real_escape_string($db_con, $_SESSION['parking-place']);
 
-                // $parkering_nei_clean = mysqli_real_escape_string($db_con, $_SESSION['parking-no']);
-
-
-                //Så forbereder vi en INSERT-spørring
-
-              $sql_booking ="INSERT INTO booking(navn,epost,telefon,rom_type_id,innsjekk_dato,utsjekk_dato,antall_rom,rom_id,antall_personer,maaltid,parkering, parkering_id)
+                //forbereder insertspørring
+              $sql_booking ="INSERT INTO booking(navn,epost,telefon,rom_type_id,innsjekk_dato,utsjekk_dato,antall_rom,rom_id,antall_personer,lunsj,middag,parkering, parkering_id)
                       VALUES(
                       '".$navn_clean."',
                       '".$epost_clean."',
-                      '".$phone_clean."'
-                      '".$kunde_id."',
+                      '".$phone_clean."',
                       '".$rom_type_clean."',
                       '".$innsjekk_clean."',
                       '".$utsjekk_clean."',
                       '".$antall_rom_clean."',
                       '".$rom_clean."',
                       '".$people_clean."',
-                      '".$mt."',
+                      '".$lunsj_clean."',
+                      '".$middag_clean."',
                       '".$parkering_clean."',
                       '".$parkering_id_clean."'
                 )";
 
                   $sett_inn = mysqli_query($db_con, $sql_booking);
 
-                    //Sjekk om spørringen ble kjørt uten feil.
+                    //Sjekk spørringsstatus
                     if($sett_inn == true) {
                         echo "<h1>Thank you! / Takk!</h1><p> Your booking is registered / Din booking er registrert.</p>";
 
