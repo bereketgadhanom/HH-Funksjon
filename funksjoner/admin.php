cdec3cca65c2073a5d36d23ef5735ec3e2f72e65
bereketgadhanom <bereketgadhanom@gmail.com>
2019-04-13 15:27:11 +0200

    metadata

--- funksjoner/admin.php
+++ funksjoner/admin.php
@@ -21,7 +21,6 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
         <link rel="stylesheet" type="text/css" href="../css/hh-stil.css">
-
         <?php include('metadata.php'); ?> <br>
     </head>
     <body id="admin-body">
